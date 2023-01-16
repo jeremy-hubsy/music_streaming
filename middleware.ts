0676d10662754ccd853e7f5e9431687d10d2917c
Jérémy Marcillaud <jeremy@syncly.fr>
2023-01-16 14:56:56 +0100

    add middleware hedge function

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+
+const signedinPages = ['/', '/playlist', 'library']
+
+export default function middleware(req){
+    
+
+    if(signedinPages.find(p => p === req.nextUrl.pathname)){
+        const token = req.cookies.TRAX_ACCESS_TOKEN
+
+        if(!token){
+            const url = req.nextUrl.clone()
+            url.pathname = '/signin'
+            return NextResponse.redirect(url)
+        }
+    }
+}
